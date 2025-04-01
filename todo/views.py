1a3dfba4524bf9b43ed6e2255334a19993f49465
Ben Miles <benmilesGM@gmail.com>
2025-04-01 11:24:40 +0100

    adds add_item.html

--- todo/views.py
+++ todo/views.py
@@ -11,3 +11,8 @@ def get_todo_list(request):
         'items' : items
     }
     return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    '''Add item to To Do list'''
+    return render(request, 'todo/add_item.html')
