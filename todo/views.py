7e1cf0e5e8db3ee272075f1c5972a7ccc1d946c7
Ben Miles <benmilesGM@gmail.com>
2025-04-01 15:35:05 +0100

    adds ItemForm to edit_item

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 '''Render HTML pages & handle form requests'''
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
@@ -30,5 +30,9 @@ def add_item(request):
 
 def edit_item(request, item_id):
     '''Edit item on To Do list'''
-
-    return render(request, 'todo/edit_item.html')
+    item = get_object_or_404(Item, id=item_id)
+    form = ItemForm(instance=item)
+    context = {
+        'form' : form
+    }
+    return render(request, 'todo/edit_item.html', context)
