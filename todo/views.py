668e2987f13a141ac505d00d56d0a609dea455ca
Ben Miles <benmilesGM@gmail.com>
2025-04-01 10:01:46 +0100

    adds item data to todo_list.html

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,13 @@
 '''Import Django Shortcuts'''
 from django.shortcuts import render
+from .models import Item
 # Create your views here.
+
+
 def get_todo_list(request):
     '''To Do homepage'''
-    return render(request, 'todo/todo_list.html')
+    items = Item.objects.all()
+    context ={
+        'items' : items
+    }
+    return render(request, 'todo/todo_list.html', context)
